c096ad530a5478e5740354513aa779b738831534
Wu Justin <justin.wu@cathayholdings.com.tw>
2022-05-15 00:49:46 +0800

    加入部署檔案

--- connect/index.js
+++ connect/index.js
@@ -7,18 +7,18 @@ dotenv.config({
 
 function init(){
   // 本地連線
-  mongoose.connect('mongodb://localhost:27017/posts')
-    .then(() => {console.log('資料庫連線成功')})
-    .catch(err => {console.log('資料庫連線失敗', err)});
-
-  // 遠端連線
-  // const DB = process.env.DATABASE.replace(
-  //   '<password>',
-  //   process.env.DATABASE_PASSWORD
-  // );
-  // mongoose.connect(DB)
+  // mongoose.connect('mongodb://localhost:27017/posts')
   //   .then(() => {console.log('資料庫連線成功')})
   //   .catch(err => {console.log('資料庫連線失敗', err)});
+
+  // 遠端連線
+  const DB = process.env.DATABASE.replace(
+    '<password>',
+    process.env.DATABASE_PASSWORD
+  );
+  mongoose.connect(DB)
+    .then(() => {console.log('資料庫連線成功')})
+    .catch(err => {console.log('資料庫連線失敗', err)});
 }
 
 module.exports = init
